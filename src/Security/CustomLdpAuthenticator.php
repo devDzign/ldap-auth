daf453bfb7bb2ee1bfc097f403bd457abd37ff07
mchabour <mourad.chabour@services.inter-invest.fr>
2023-06-01 22:05:19 +0200

    CustomAuthenticator

--- src/Security/CustomLdpAuthenticator.php
+++ src/Security/CustomLdpAuthenticator.php
@@ -9,6 +9,10 @@ use App\Repository\UserRepository;
 use App\Service\ActiveDirectory;
 use Doctrine\ORM\EntityManagerInterface;
 use Lexik\Bundle\JWTAuthenticationBundle\Encoder\JWTEncoderInterface;
+use Lexik\Bundle\JWTAuthenticationBundle\Event\JWTNotFoundEvent;
+use Lexik\Bundle\JWTAuthenticationBundle\Exception\JWTEncodeFailureException;
+use Lexik\Bundle\JWTAuthenticationBundle\Exception\MissingTokenException;
+use Lexik\Bundle\JWTAuthenticationBundle\Response\JWTAuthenticationFailureResponse;
 use Symfony\Component\HttpFoundation\JsonResponse;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
@@ -22,9 +26,10 @@ use Symfony\Component\Security\Http\Authenticator\Passport\SelfValidatingPasspor
 use Symfony\Component\Security\Http\EntryPoint\AuthenticationEntryPointInterface;
 use Gesdinet\JWTRefreshTokenBundle\Generator\RefreshTokenGeneratorInterface;
 use Gesdinet\JWTRefreshTokenBundle\Model\RefreshTokenManagerInterface;
-class CustomLdpAuthenticator extends AbstractAuthenticator implements AuthenticationEntryPointInterface
-{
 
+
+class CustomLdapAuthenticator extends AbstractAuthenticator implements AuthenticationEntryPointInterface
+{
     /* AUTOWIRING DES OBJETS PAR SYMFONY */
     public function __construct(
         private ActiveDirectory $activeDirectory,
@@ -48,7 +53,7 @@ class CustomLdpAuthenticator extends AbstractAuthenticator implements Authentica
     public function authenticate(Request $request): SelfValidatingPassport
     {
         /* TEST SI LE CONTENT-TYPE EST OK */
-        if ('json' != $request->getContentTypeFormat() || null == $request->getContentTypeFormat()) {
+        if ('json' !== $request->getContentTypeFormat() || null === $request->getContentTypeFormat()) {
             throw new CustomUnsupportedMediaTypeException('WRONG CONTENT-TYPE');
         }
 
@@ -79,21 +84,86 @@ class CustomLdpAuthenticator extends AbstractAuthenticator implements Authentica
                     throw new AuthenticationException();
                 }
                 $this->entityManager->persist($userToPersist);
+            } else { /* SI OUI, ON METS UNIQUEMENT A JOUR SA DATE DE DERNIERE CONNEXION */
+                $userFromRepo->setLastLogin(new \DateTime());
+                $this->entityManager->persist($userFromRepo);
             }
+            $this->entityManager->flush();
+        }
+        /* SI TOUT S'EST BIEN PASSE ON RENVOIE UN BADGE AUTORISÉ A PASSER DANS SYMFONY */
+        return new SelfValidatingPassport(new UserBadge($loginFromRequest));
     }
 
+    /* CETTE FONCTION EST APPELLÉE SI L'AUTHENTIFICATION A RÉUSSIE */
     public function onAuthenticationSuccess(Request $request, TokenInterface $token, string $firewallName): ?Response
     {
-        return new JsonResponse('');
+        /* ON RECUPERE LES ROLES DE L'UTILISATEUR A PARTIR DE LA BDD */
+        try {
+            $user = $this->userRepository->findOneBy(['login' => $token->getUserIdentifier()]);
+            /* RECUPERE LES ROLES DE L'UTILISATEUR */
+            if (null !== $user) {
+                $roles = $user->getRoles();
+            } else {
+                $roles = null;
+            }
+
+            /* ON GENERE LE TOKEN AVEC UNE DUREE DE VIE DE 1200s (20mn) */
+            $jwtToken = $this->encoder->encode(['username' => $token->getUserIdentifier(), 'roles' => $roles, 'id' => $user->getId(), 'exp' => time() + 1200]);
+
+            /* ON VA VERIFIER QU'UN REFRESH_TOKEN N'EXISTE PAS DEJA ET SI C'EST LE CAS ON LE RETIRE */
+
+            $refreshToken = $this->refreshTokenManager->getLastFromUsername($token->getUserIdentifier());
+            if( null !== $refreshToken) {
+                $this->refreshTokenManager->delete($refreshToken);
+            }
+
+            /* ON GENERE LE REFRESH TOKEN AVEC UNE DUREE DE VIE DE 30 JOURS */
+            $refreshToken = $this->refreshTokenGenerator->createForUserWithTtl($token->getUser(), 2592000);
+
+            /* ON PERSISTE LE REFRESH TOKEN EN BASE */
+            $this->entityManager->persist($refreshToken);
+
+            /* ON ENREGISTRE TOUS LES TOKENS */
+            $this->entityManager->flush();
+        } catch (JWTEncodeFailureException $JWTEncodeFailureException) {
+            return new JsonResponse(['message' => $JWTEncodeFailureException->getMessage()], Response::HTTP_INTERNAL_SERVER_ERROR);
+        } catch (\Exception $e) {
+            return new JsonResponse(['message' => $e->getMessage()], Response::HTTP_INTERNAL_SERVER_ERROR);
+        }
+
+        /* ON RENVOIE LE TOKEN A L'UTILISATEUR */
+        return new JsonResponse(['token' => $jwtToken, 'refresh_token' => $refreshToken->getRefreshToken()], Response::HTTP_CREATED);
     }
 
+    /* CETTE FONCTION EST APPELLEE SI L'AUTHENTIFICATION A ÉCHOUÉE */
     public function onAuthenticationFailure(Request $request, AuthenticationException $exception): ?Response
     {
-        return new JsonResponse('');
+        /* ON GENERE L'ERREUR DE FACON DYNAMIQUE */
+        if ($exception instanceof CustomUnsupportedMediaTypeException) {
+            $codeResponse = Response::HTTP_UNSUPPORTED_MEDIA_TYPE;
+        } elseif ($exception instanceof CustomBadRequestException) {
+            $codeResponse = Response::HTTP_BAD_REQUEST;
+        } elseif ($exception instanceof UserNotFoundException) {
+            $codeResponse = Response::HTTP_NOT_FOUND;
+        } else {
+            $codeResponse = Response::HTTP_UNAUTHORIZED;
+        }
+        $data = [
+            /* ON RECUPERE LE MESSAGE D'EXCEPTION */
+            'message' => strtr($exception->getMessageKey(), $exception->getMessageData()),
+        ];
+
+        /* ON RETOURNE LE MESSAGE D'ERREUR A L'UTILISATEUR */
+        return new JsonResponse($data, $codeResponse);
     }
 
-    public function start(Request $request, AuthenticationException $authException = null)
+    /* CETTE FONCTION EST APPELLÉE SI L'UTILISATEUR CONSOMME UNE ROUTE PROTEGEE, SANS FOURNIR D'INFORMATION D'AUTHENTIFICATION */
+    public function start(Request $request, AuthenticationException $authException = null): ?Response
     {
-        // TODO: Implement start() method.
+        $exception = new MissingTokenException('JWT Token not found', 0, $authException);
+        $event = new JWTNotFoundEvent($exception, new JWTAuthenticationFailureResponse($exception->getMessageKey()));
+        return $event->getResponse();
     }
 }
+
+
